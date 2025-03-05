7bb5c8bf9833d8b6005ba3bd7a4450c21d97b817
Dmytro Onishchenko <donischenko801@gmail.com>
2025-03-05 16:57:12 +0200

    WIP

--- core/src/config/importRegister.ts
+++ core/src/config/importRegister.ts
@@ -1,3 +1,2 @@
 export const importRegister = {pluginExampleReact18: typeof window !== 'undefined' ? import('pluginExampleReact18') : Promise.resolve(null),
-pluginExampleReact19: typeof window !== 'undefined' ? import('pluginExampleReact19') : Promise.resolve(null),
-somePlugin: typeof window !== 'undefined' ? import('somePlugin') : Promise.resolve(null),}
+pluginExampleReact19: typeof window !== 'undefined' ? import('pluginExampleReact19') : Promise.resolve(null),}
